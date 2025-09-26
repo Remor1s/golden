5f8214420b02a2d858a7b78578cc7a00d7603e82
Remor1s <168758957+Remor1s@users.noreply.github.com>
2025-09-26 19:15:00 +0300

    fix: hardcode base path for gh-pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
+  base: '/golden/',
   publicDir: '../images',
   plugins: [react()],
   server: { port: 8080, host: true },
